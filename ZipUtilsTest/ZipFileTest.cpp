d466e3da68bf5fa18a5d50d5daa7892429a14405
Gerhard <geh@geherbert.de>
2013-12-18 05:24:42 +0100

    rename

---
+++ ZipUtilsTest/ZipFileTest.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "ZipFile.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ZipUtilsTest
+{		
+  TEST_CLASS(ZipFileTest)
+  {
+  public:
+
+    TEST_METHOD(FirstTest)
+    {
+      ZipFile zipFile(L"c:\\temp\\Test.zip");
+    }
+
+  };
+}
